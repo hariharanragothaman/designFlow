c4136a5acda98c2deee688f66f0be7b523393223
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2022-03-28 05:25:52 -0400

    Adding borg state examples

---
+++ design_patterns/pythontalk2/8_monostate_botg_state_pattern.py
@@ -0,0 +1,32 @@
+"""
+> Monostate Pattern: Borg
+"""
+from log import logger
+
+
+class SharedAllMixin:
+    def __init__(self, *args, **kwargs):
+        try:
+            self.__class__._attributes
+        except AttributeError:
+            self.__class__._attributes = {}
+
+        self.__dict__ = self.__class__._attributes
+        super().__init__(*args, **kwargs)
+
+
+class BaseFetcher:
+    def __init__(self, source):
+        self.source = source
+
+
+class TagFetcher(SharedAllMixin, BaseFetcher):
+    def pull(self):
+        logger.info("pulling from tag %s", self.source)
+        return f"Tag = {self.source}"
+
+
+class BranchFetcher(SharedAllMixin, BaseFetcher):
+    def pull(self):
+        logger.info("pulling from branch %s", self.source)
+        return f"Branch = {self.source}"
